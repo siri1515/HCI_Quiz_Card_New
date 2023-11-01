cda3c7bccc197123803badcd61fe3b65c718a9a8
siri1515 <siri91445@gmail.com>
2023-11-01 10:41:05 -0400

    ai function added

--- pages/ai.js
+++ pages/ai.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import styles from "./ai.module.css";
 import NavBar from "../components/Common/NavBar";
+import { useAppContext } from '../context/AppContext';
+import { useRouter } from 'next/router';
 
 export default function AI() {
   const [userInput, setUserInput] = useState("");
-  const [result, setResult] = useState();
+  const [title, setTitle] = useState("");
+  const { list, setList } = useAppContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
   async function onSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
     try {
       const response = await fetch("/api/openai", {
         method: "POST",
@@ -22,13 +28,24 @@ export default function AI() {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
+      const newCardSet = {
+        title: title,
+        id: Math.random().toString(),
+        cards: data.result
+      };
+      setList((prevCardSet) => {
+        return [newCardSet, ...prevCardSet];
+      })
       console.log(data.result);
-      setResult(data.result);
       setUserInput("");
+      router.replace('/cardset');
+      
     } catch(error) {
-      // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+    }
+    finally {
+      setIsLoading(false);
     }
   }
 
@@ -36,18 +53,31 @@ export default function AI() {
     <div>
       <NavBar />
       <main className={styles.main}>
-        <h3>Create Cardset</h3>
+        <h3>Let me create cardsets for you!</h3>
+        <p>Input your learning material and I will automically generate a cardset including 3 cards for you</p>
         <form onSubmit={onSubmit}>
           <input
             type="text"
+            name="title"
+            placeholder="Enter your New Cardset Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
             name="info"
             placeholder="Enter your Learning Material"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
-          />
+            className="textarea_input"
+          ></textarea>
           <input type="submit" value="Generate cards" />
         </form>
-        <div className={styles.result}>{result}</div>
+        {isLoading && 
+          <div className={styles.overlay}>
+            <h1 className={styles.loading_title}><b>Please wait a moment</b></h1>
+            <div className={styles.loading_image}></div>
+          </div>
+        }
       </main>
     </div>
   );
