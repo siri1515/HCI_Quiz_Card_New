a2ea844db7a933d78d5d027aa704c9aaf1a219fd
siri1515 <siri91445@gmail.com>
2023-11-03 14:16:02 -0400

    final version

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -27,8 +27,8 @@ export default async function (req, res) {
   try {
     const chatCompletion = await openai.chat.completions.create({
       messages: [
-                  { role: "system", content: "You need to create as many cards as you think are appropriate(but no more than 10 cards) and their corresponding answers based on the learning resources provided by the user. Please ensure each question and answer is wrapped in double quotes, separated by a comma. The entire pair should be enclosed in single quotes, formatted as: '['<question>', '<answer>']'. Ensure answers are kept simple and straightforward."},
-                  { role: "user", content: info+"You need to create as many cards as you think are appropriate(but no more than 10 cards) and their corresponding answers based on the learning resources provided by the user. Please ensure each question and answer is wrapped in double quotes, separated by a comma. The entire pair should be enclosed in single quotes, formatted as: '['<question>', '<answer>']'. Ensure answers are kept simple and straightforward."}
+                  { role: "system", content: "You need to create questions and their corresponding answers based on the learning resources provided by the user. Please ensure each question and answer is wrapped in double quotes, separated by a comma. The entire pair should be enclosed in single quotes, formatted as: '['<question>', '<answer>']'. Ensure answers are kept simple and straightforward."},
+                  { role: "user", content: info+"You need to create questions and their corresponding answers based on the learning resources provided by the user. Please ensure each question and answer is wrapped in double quotes, separated by a comma. The entire pair should be enclosed in single quotes, formatted as: '['<question>', '<answer>']'. Ensure answers are kept simple and straightforward." }
                 ],
       model: "gpt-4",
     });
@@ -38,7 +38,8 @@ export default async function (req, res) {
     console.log("match: ", matches);
     if (matches) {
       matches.forEach(item => {
-          const parsedArray = JSON.parse(item.replace(/`/g, '"')); //replace either ` or ' to ""
+        const parsedArray = JSON.parse(item.replace(/`/g, '"').replace(/\\'/g, "'"));
+        //replace either ` to ""， \\ to '
           console.log("parsedArray: ", parsedArray);
           questionsAndAnswers.push({
               id: Math.random().toString(),
@@ -51,6 +52,7 @@ export default async function (req, res) {
 
     res.status(200).json({ result: questionsAndAnswers });
   } catch(error) {
+    alert('something wrong with chatgpt, please try again');
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
